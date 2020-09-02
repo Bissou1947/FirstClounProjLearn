dd71f0c658df3b4e50ac9bc3ffffa28621750057
Bissou <bassil_1947@hotmail.com>
2020-09-02 17:35:28 +0300

    Add MVC to App

---
+++ FirstClounProj/FirstClounProj/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstClounProj.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index() {
+            return "hi from home index method.";
+        }
+    }
+}
--- FirstClounProj/FirstClounProj/Startup.cs
+++ FirstClounProj/FirstClounProj/Startup.cs
@@ -16,6 +16,9 @@ namespace FirstClounProj
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc(); .......................for mvc architict
+            //services.AddControllers(); ............... for API architict
+            services.AddControllersWithViews(); //........use this for now
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +31,51 @@ namespace FirstClounProj
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints(Endpoint=> {
+                Endpoint.MapDefaultControllerRoute();
             });
+
+            //app.Use(async (context,next) => {
+            //    await context.Response.WriteAsync("First");
+            //    await next();
+            //    await context.Response.WriteAsync(" First First");
+            //});
+            //app.Use(async (context, next) => {
+            //    await context.Response.WriteAsync(" Second");
+            //    await next();
+            //    await context.Response.WriteAsync(" Second Second");
+            //});
+            //app.Use(async (context, next) => {
+            //    await context.Response.WriteAsync(" Third");
+            //    await next();
+            //});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/", async context =>
+            //    {
+            //        //await context.Response.WriteAsync(env.EnvironmentName);
+
+            //        if (env.IsDevelopment())
+            //        {
+            //            await context.Response.WriteAsync("IsDevelopment");
+            //        }
+            //        else if (env.IsEnvironment("Production"))
+            //        {
+            //            await context.Response.WriteAsync("Yes it i Production");
+            //        }
+
+            //    });
+            //});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/Finish", async context =>
+            //    {
+            //        await context.Response.WriteAsync("finish");
+            //    });
+            //});
+
         }
     }
 }
